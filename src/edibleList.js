985987d7d7b69410ca5e55f15560706a6bff727a
Tara <talsaidi@ucdavis.edu>
2023-02-06 11:45:27 -0800

    created dropdown, not functional. created getLatinFilterResults and pullFilteredObservations, not functional

---
+++ src/edibleList.js
@@ -0,0 +1,43 @@
+const edibleListCommon = [
+  "salmonberry",
+  "borage",
+  "wood sorrel",
+  "pickleweed",
+  "chanterelle",
+  "arrowleaf balsamroot",
+];
+
+const edibleList = [
+  {
+    id: 1,
+    value: ["Rubus spectabilis"],
+    label: "Salmonberry",
+  },
+  {
+    id: 2,
+    value: ["Borago officinalis"],
+    label: "Borage",
+  },
+  {
+    id: 3,
+    value: ["Oxalis articulata", "Oxalis corniculata"],
+    label: "Wood Sorrel",
+  },
+  {
+    id: 4,
+    value: "34b2b58a-0123-49e2-b2de-1eef0922139b",
+    label: "pickleweed",
+  },
+  {
+    id: 5,
+    value: ["Cantharellus cibarius", "Cantharellus formosus"],
+    label: "chanterelle",
+  },
+  {
+    id: 6,
+    value: "b68c6b76-bbe0-49a6-8a90-209b8d88d353",
+    label: "arrowleaf balsamroot",
+  },
+];
+
+export default edibleList;
