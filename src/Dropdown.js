e164f017a4920eac37e37cfb6b90904079658cb1
Sarah Sanborn <sanbornsarahn@gmail.com>
2023-02-08 09:11:39 -0800

    Added done button on dropdown. Function call needed! Also updated CSS more

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,47 +1,82 @@
 // import * as React from 'react';
 import React, { useState, useEffect } from "react";
-import { MultiSelect } from "react-multi-select-component";
+// import { MultiSelect } from "react-multi-select-component";
 import Button from "@mui/material/Button";
-import Multiselect from "react-widgets/Multiselect";
+// import Multiselect from "react-widgets/Multiselect";
 import { AiOutlineDown } from 'react-icons/ai'
 
 import edibleList from "./edibleList";
 
 const Dropdown = () => {
-  const [selectedPlants, setSelectedPlants] = useState([]);
+  const [selectedSpecies, setSelectedSpecies] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleSelection = plant => {
-    if (selectedPlants.includes(plant)) {
-      setSelectedPlants(selectedPlants.filter(selectedPlant => selectedPlant !== plant));
+  const handleSelection = species => {
+    if (selectedSpecies.includes(species)) {
+      setSelectedSpecies(selectedSpecies.filter(selectedSpecies => selectedSpecies !== species));
     } else {
-      setSelectedPlants([...selectedPlants, plant]);
+      setSelectedSpecies([...selectedSpecies, species]);
     }
+  };
+
+  const handleDone = () => {
+    setIsOpen(false);
+    // Function call for API here
   };
 
   return (
     <div className="dropdown-container">
-      <Button className="dropdown-button"  onClick={() => setIsOpen(!isOpen)}>
-        <span className="select-species-button"> Select a species... <AiOutlineDown /> </span>
-      </Button>
+      <button className={`select-species-button ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+        Select a Species...
+        <span><AiOutlineDown></AiOutlineDown></span>
+      </button>
       {isOpen && (
         <ul className="dropdown-list">
-          {edibleList.map(plant => (
-            <li key={plant.id} className="dropdown-list-item">
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedPlants.includes(plant)}
-                  onChange={() => handleSelection(plant)}
-                />
-                {plant.label}
-              </label>
+          {edibleList.map(species => (
+            <li key={species.id} className='dropdown-list-item'>
+              <input
+                type='checkbox'
+                checked={selectedSpecies.includes(species)}
+                onChange={() => handleSelection(species)}
+              />
+              {species.label}
             </li>
           ))}
+          <li className="done-button-list-item">
+            <button className="done-button" onClick={handleDone}>Done</button>
+          </li>
         </ul>
       )}
     </div>
   );
+};
+
+
+  
+
+  // return (
+  //   <div className="dropdown-container">
+  //     <button className="dropdown-button"  onClick={() => setIsOpen(!isOpen)}>
+  //       <span className="select-species-button"> Select a species... <AiOutlineDown /> </span>
+  //     </button>
+  //     {isOpen && (
+  //       <ul className="dropdown-list">
+  //         {edibleList.map(species) => (
+  //           <li key={species.id} className="dropdown-list-item">
+  //             <label>
+  //               <input
+  //                 type="checkbox"
+  //                 checked={selectedSpecies.includes(species)}
+  //                 onChange={() => handleSelection(species)}
+  //               />
+  //               {species.label}
+  //             </label>
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     )}
+  //   </div>
+  // );
 
   // const [selectedOptions, setSelectedOptions] = useState([]);
   // const [selectedKeys, setSelectedKeys] = useState([]);
@@ -64,6 +99,6 @@ const Dropdown = () => {
   //     />
   //   </li>
   // );
-};
+// };
 
 export default Dropdown;
